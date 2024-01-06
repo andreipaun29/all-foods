0e8742456f7583094f087954299b30b79a999e93
Andrei Paun <andrei.paun1@s.unibuc.ro>
2024-01-06 15:57:05 +0200

    Added home and account components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { AccountComponent } from './components/account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
+    HomeComponent,
+    AccountComponent,
   ],
   imports: [
     BrowserModule,
