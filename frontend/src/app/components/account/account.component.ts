7a3424b90ebfc9bc0039763419d1a9ca2a4bb382
Andrei Paun <andrei.paun1@s.unibuc.ro>
2024-01-29 21:15:23 +0200

    Added user account - no location

--- frontend/src/app/components/account/account.component.ts
+++ frontend/src/app/components/account/account.component.ts
@@ -3,17 +3,52 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.scss'],
 })
 export class AccountComponent implements OnInit {
-  userId: number = 0;
 
-  constructor(private route: ActivatedRoute, private authService: AuthService) {}
+  constructor(private router: Router) {}
+
+  firstName: string = localStorage.getItem('firstName') || '';
+  lastName: string = localStorage.getItem('lastName') || '';
+  email: string = localStorage.getItem('email') || '';
+  password: string = '';
+
+
+
+  picture: string = '../../assets/profile.jpg'
 
   ngOnInit(): void {
 
+    let pLength = localStorage.getItem('password')?.length;
+    this.password = '*'.repeat(pLength || 0);
   }
+
+
+  logout(){
+    this.router.navigate(['/login']);
+  }
+
+  home(){
+    this.router.navigate(['/home']);
+  }
+
+
+  showPassword = false;
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+    if(this.showPassword){
+      this.password = localStorage.getItem('password') || '';
+    }
+    else{
+      let length = this.password.length;
+      this.password = '*'.repeat(length);
+    }
+  }
+  
 }
